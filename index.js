5ff3d9ef0c7c4fb9a6b7edcb54f04b0e59c8270e
wlytle <wlytle89@gmail.com>
2020-11-25 17:07:45 -0700

    commit tree 0306b845521a0bbcfd14de3063552f81cafd6f03

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Contutorialuctor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const titleCase = (letter) => {
+    return letter.toUpperCase();
+  };
+  return tutorials.map((title) => {
+    return title.replace(/\b(\w)/g, titleCase);
+  });
+};
